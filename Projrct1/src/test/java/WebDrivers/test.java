ec70cdd6aff664effa1d2068be083075ae4a2118
ssinghvi8696 <ssinghvi8696@shubham-singhvi.asia-south2-a.c.golden-bloom-320713.internal>
2021-07-27 11:41:50 +0000

    Testerf

---
+++ Projrct1/src/test/java/WebDrivers/test.java
@@ -0,0 +1,26 @@
+package WebDrivers;
+
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class test {
+
+	@Test 
+	
+	public void date() 
+	{
+		System.setProperty("webdriver.chrome.driver","C:\\Drivers\\chromedriver_win32\\chromedriver.exe");
+
+
+		WebDriver date = new ChromeDriver();
+
+		date.get("http://demo.automationtesting.in/Datepicker.html");
+		
+		JavascriptExecutor JSE = (JavascriptExecutor) date;
+		
+		JSE.executeScript("document.getElementById('datepicker1').value='29/07/2010'");	
+	} 
+}
